8506c764aff0d2972348e311c88f1264153ff6db
yanliang <negitester@gmail.com>
2021-11-25 16:38:32 +0900

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const Chess = (props) => {
+  const classType = {
+    "0": "plain",
+    "1": "white-chess",
+    "2": "black-chess"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <span className={classType[props.value]} />
+  )
+}
+
+const PrintCell = (props) => {
+  return (
+    <div className="cell" onClick={() => props.clEvent(props.x, props.y)}>
+      <Chess value={props.value} />
     </div>
+  )
+}
+
+const PrintBoard = (props) => {
+  return (
+    <div id="board">
+      {
+        props.board.map((element, i) => {
+          return (
+            element.map((it, j) => {
+              return (
+                <PrintCell value={it} key={String(i)+String(j)} x={i} y={j} clEvent={props.clEvent}/>
+              )
+            })
+          )
+        })
+      }
+    </div>
+  )
+}
+
+const App = () => {
+  const initBoard = [
+    ["0", "0", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"]
+  ]
+
+  const [cb, setCb] = useState(initBoard)
+  const [t, setT] = useState(1)
+
+  const playChess = (x, y) => {
+    console.log(x, y)
+    let newBoard = cb.map((el) => el)
+    newBoard[x][y] = String(t)
+    let newTurn = 0
+    if (t === 1) {
+      newTurn = 2
+    } else {
+      newTurn = 1
+    }
+    setCb(newBoard)
+    setT(newTurn)
+  }
+  return (
+    <PrintBoard board={cb} turn={t} clEvent={playChess}/>
   );
 }
 
